ae111be8e71bc37f0ba6c8d41e1d1ba8539ea405
whata <waku.hatakeyama@gmail.com>
2022-05-18 05:57:05 +0000

    Modify code for aws without container

--- src/train.py
+++ src/train.py
@@ -35,7 +35,10 @@ if __name__ == "__main__":
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    dataset = AirbusDS(dataset_root="/workspace/dataset", aug=True)
+    dataset = AirbusDS(
+        dataset_root="/home/ubuntu/workspace/Airbus_ship/dataset",
+        aug=True
+    )
     train_dataloader = get_dataloader(dataset)
     model.train()
     model.to(device)
