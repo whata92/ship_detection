01b8e54d2820ec719b7aa6be1396b07735ff6677
whata <waku.hatakeyama@gmail.com>
2022-08-07 05:30:48 +0000

    Implement baseline model of faster rcnn

--- src/dataloader/augmentation.py
+++ src/dataloader/augmentation.py
@@ -2,7 +2,7 @@ import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
 
-def get_train_transforms():
+def default_augmentation():
     return A.Compose(
         [
             # A.RandomSizedCrop(
@@ -11,25 +11,25 @@ def get_train_transforms():
             #     width=1024,
             #     p=0.5
             # ),
-            A.OneOf(
-                [
-                    A.HueSaturationValue(
-                        hue_shift_limit=0.2,
-                        sat_shift_limit=0.2,
-                        val_shift_limit=0.2,
-                        p=0.9
-                    ),
-                    A.RandomBrightnessContrast(
-                        brightness_limit=0.2,
-                        contrast_limit=0.2,
-                        p=0.9
-                    ),
-                ],
-                p=0.9
-            ),
-            A.ToGray(p=0.01),
-            A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.5),
+            # A.OneOf(
+            #     [
+            #         A.HueSaturationValue(
+            #             hue_shift_limit=0.2,
+            #             sat_shift_limit=0.2,
+            #             val_shift_limit=0.2,
+            #             p=0.9
+            #         ),
+            #         A.RandomBrightnessContrast(
+            #             brightness_limit=0.2,
+            #             contrast_limit=0.2,
+            #             p=0.9
+            #         ),
+            #     ],
+            #     p=0.9
+            # ),
+            # A.ToGray(p=0.01),
+            # A.HorizontalFlip(p=0.5),
+            # A.VerticalFlip(p=0.5),
             A.Resize(height=512, width=512, p=1),
             A.augmentations.transforms.Normalize(p=1.0),
             # A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
@@ -40,22 +40,14 @@ def get_train_transforms():
             format='pascal_voc',
             min_area=0,
             min_visibility=0,
-            label_fields=['labels']
+            label_fields=['category_id']
         )
     )
 
 
-def get_valid_transforms():
-    return A.Compose(
-        [
-            A.Resize(height=512, width=512, p=1.0),
-            ToTensorV2(p=1.0),
-        ],
-        p=1.0,
-        bbox_params=A.BboxParams(
-            format='pascal_voc',
-            min_area=0,
-            min_visibility=0,
-            label_fields=['labels']
-        )
-    )
+def train_augmentation():
+    raise NotImplementedError()
+
+
+def val_augmentation():
+    raise NotImplementedError()
