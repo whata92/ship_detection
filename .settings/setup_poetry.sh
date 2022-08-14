70a27adf8cd5a87e64163c86e09555c9a19ee61f
whata92 <waku.hatakeyama@gmail.com>
2022-08-14 01:27:15 +0000

    Implement poetry setup bash file

---
+++ .settings/setup_poetry.sh
@@ -0,0 +1,39 @@
+sudo apt install -y \
+    gcc \
+    make \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    curl \
+    llvm \
+    libncurses5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    liblzma-dev
+
+# Insall pyenv
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+source ~/.bash_profile
+
+pyenv install 3.9.13
+pyenv global 3.9.13
+
+
+# Install poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+echo 'export PATH=$PATH:$HOME/.poetry/env' >> ~/.bash_profile
+source ~/.bash_profile
+poetry install
