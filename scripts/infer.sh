c7022a5b87f2a56b6b9001895011da94167a5b78
whata92 <waku.hatakeyama@gmail.com>
2022-09-04 11:39:09 +0000

    Modify sh file setting

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,5 +1,5 @@
 python src/tools/infer.py \
     --img_dir /home/ubuntu/workspace/ship_detection/dataset/inference/2022-08-27_sentinel1 \
-    --output_dir output/yolof/inference \
-    --model_cfg output/yolof_r50_c5_8x8_1x_coco.py \
-    --checkpoint output/latest.pth \
+    --output_dir output/faster_rcnn/inference \
+    --model_cfg output/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py \
+    --checkpoint output/faster_rcnn/latest.pth \
