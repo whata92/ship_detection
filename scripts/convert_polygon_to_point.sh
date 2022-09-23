a9b64fd480f020ae3872dad82707e636dd3e118f
whata92 <waku.hatakeyama@gmail.com>
2022-09-23 11:15:06 +0000

    Implement polygon to point function

---
+++ scripts/convert_polygon_to_point.sh
@@ -0,0 +1,3 @@
+python src/tools/convert_polygon_to_point.py \
+    --geojson /home/ubuntu/workspace/ship_detection/output/faster_rcnn/inference/2022-08-27_sentinel1.geojson \
+    --out_geojson /home/ubuntu/workspace/ship_detection/output/faster_rcnn/inference/2022-08-27_sentinel1_point.geojson
